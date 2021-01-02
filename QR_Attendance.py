55b79d4205dbeb21367b85f45ade59db1fd86b4e
harshmangal1108 <harsh.mangal1108@gmail.com>
2021-01-02 10:07:42 +0530

    QR Attendance with MongoDB Complete

--- QR_Attendance.py
+++ QR_Attendance.py
@@ -1,57 +1,68 @@
+from pymongo import MongoClient
 import cv2
 import numpy as np
 import pyzbar.pyzbar as pyzbar
 import sys
+import datetime
 import time
+
+
 # starting the webcam using opencv
 cap = cv2.VideoCapture(0)
-
-fob = open('attendence.txt', 'w+')
-
 names = []
 
-# function for writing the data into text file
+# function for writing the data into Mongo DB
+# Mongo Initialization
+client = MongoClient("mongodb://localhost:27017/")
+database = client.Attendance
+collection = database.Lab
 
 
-def enterData(z):
-    if z in names:
+def enterData(Name, Date, Subject):
+    if Name in names:
         pass
     else:
-        names.append(z)
+        names.append(Name)
         print(names)
-        z = ''.join(str(z))
-        fob.write(z+'\n')
+        Name = ''.join(str(Name))
+
+        insertion = collection.insert_one(
+            {"Name": Name, "Date": Date, "Subject": Subject})
+        if insertion.acknowledged:
+            print("Document is added and ID is: ", insertion.inserted_id)
     return names
 
 
-print("<------------------------->")
-subject = str(input("Enter Name of Subject : "))
-print('<------ Reading ---------->')
-# function for check the data is present or not
+def QR_attendance():
+
+    print("<------------------------->")
+    Subject = str(input("Enter Name of Subject : "))
+    Date = datetime.datetime.today()
+    print('<------ Reading ---------->')
+
+    # function for check the data is present or not
+    def checkData(data):
+        data = str(data)
+        if data in names:
+            print(data + ' is already present')
+        else:
+            print('\n'+str(len(names)+1)+'\n'+data)
+            enterData(data, Date, Subject)
+
+    while True:
+        _, frame = cap.read()
+        decodedObjects = pyzbar.decode(frame)
+        for obj in decodedObjects:
+            Name = obj.data.decode("utf-8")
+            checkData(Name)
+            time.sleep(0.5)
+
+        cv2.imshow("Frame", frame)
+
+        # closing the program when s is pressed
+        if cv2.waitKey(1) & 0xFF == ord('c'):
+            cv2.destroyAllWindows()
+            break
 
 
-def checkData(data):
-    data = str(data)
-    if data in names:
-        print('Already Present')
-    else:
-        print('\n'+str(len(names)+1)+'\n'+data)
-        enterData(data)
-
-
-while True:
-    _, frame = cap.read()
-    decodedObjects = pyzbar.decode(frame)
-    for obj in decodedObjects:
-        Name = obj.data.decode("utf-8")
-        checkData(Name)
-        time.sleep(1)
-
-    cv2.imshow("Frame", frame)
-
-    # closing the program when s is pressed
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.destroyAllWindows()
-        break
-
-fob.close()
+QR_attendance()
