ba02b632a241bab154ae3c97a7ecae8429ed06c7
Harsh Mangal <harsh.mangal1108@gmail.com>
2021-02-05 02:27:21 +0530

    Files setup

---
+++ python/QR/Main.py
@@ -0,0 +1,10 @@
+"""
+This File will be main everything will be called from here!
+User have to run this file for the software to work!
+"""
+import os
+print("<------- Options -------->")
+option = input(" 1. QR Attendance ")
+
+if option == '1':
+    os.system("python QR_Attendance.py")
